b3fa433a6e11c5fc83b42fb3603f29a334bfa8e1
Jim Moore <moore.jim@gmail.com>
2018-07-04 06:22:38 -0600

    Simple client/server examples

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,67 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+
+import axios from 'axios';
+
+class CallDisplay extends React.Component {
+  state = {
+    msg: "UNINITIALIZED"
+  }
+
+  componentDidMount() {
+    this.refreshCall()
+  }
+
+  render() {
+    return (
+      <div>
+        <p>{this.state.msg}</p><br/>
+        <button onClick={this.refreshCall}>Refresh</button><br/>
+        <button onClick={this.refreshEtag}>ETag</button>
+        <button onClick={this.refreshLastModified}>Last Modified</button><br/>
+        <button onClick={this.refreshEtagLastModified}>ETag and Last Modified</button><br/>
+        <button onClick={this.refreshEtag1m}>ETag max-age 1m</button>
+        <button onClick={this.refreshLastModified1m}>Last Modified max-age 1m</button><br/>
+      </div>
+    )
+  }
+
+  refreshCall = () => {
+    axios.get('http://localhost:8080/').then(this.refreshResp)
+  }
+
+  refreshEtag = () => {
+    axios.get('http://localhost:8080/etag').then(this.refreshResp)
+  }
+
+  refreshLastModified = () => {
+    axios.get('http://localhost:8080/last-modified').then(this.refreshResp)
+  }
+
+  refreshEtagLastModified = () => {
+    axios.get('http://localhost:8080/etag-last-modified').then(this.refreshResp)
+  }
+
+  refreshEtag1m = () => {
+    axios.get('http://localhost:8080/etag-1m').then(this.refreshResp)
+  }
+
+  refreshLastModified1m = () => {
+    axios.get('http://localhost:8080/last-modified-1m').then(this.refreshResp)
+  }
+
+  refreshResp = (res) => {
+      this.setState({msg: res.data});
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-intro">
+          <CallDisplay/>
+        </div>
       </div>
     );
   }
--- server/src/main/kotlin/com/example/demo/DemoApplication.kt
+++ server/src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -1,11 +1,156 @@
 package com.example.demo
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.ServletComponentScan
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.CacheControl
+import org.springframework.http.HttpHeaders.IF_MODIFIED_SINCE
+import org.springframework.http.HttpHeaders.IF_NONE_MATCH
+import org.springframework.http.HttpStatus.NOT_MODIFIED
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.context.request.ServletWebRequest
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import java.time.Instant
+import java.util.concurrent.TimeUnit.MINUTES
 
+@ServletComponentScan
 @SpringBootApplication
-class DemoApplication
+open class DemoApplication
+
+@RestController
+@Suppress("unused")
+class HelloController {
+    private val myEtag = "an_etag_header"
+    private val resourceLastModified = Instant.parse("2018-07-04T13:15:30.00Z").toEpochMilli()
+
+    @RequestMapping("/")
+    fun index() = "NOTHING"
+
+    @RequestMapping("/etag")
+    fun etag(swr: ServletWebRequest): ResponseEntity<String> {
+        LOG.info("GET /etag with {} of {}", IF_NONE_MATCH, swr.getHeader(IF_NONE_MATCH))
+        return if (!swr.checkNotModified(myEtag)) {
+            LOG.info("No match: returning \"fresh\" 200 data")
+            ResponseEntity.ok()
+                    .eTag(myEtag).cacheControl(CacheControl.noCache())
+                    .body("Greetings with ETag $myEtag at ${Instant.now()}")
+        } else {
+            LOG.info("Match: returning 304")
+            ResponseEntity.status(NOT_MODIFIED).build()
+        }
+    }
+
+    @RequestMapping("/last-modified")
+    fun lastModified(swr: ServletWebRequest): ResponseEntity<String> {
+        LOG.info("GET /last-modified with {} of {}", IF_MODIFIED_SINCE, swr.getHeader(IF_MODIFIED_SINCE))
+        return if (!swr.checkNotModified(resourceLastModified)) {
+            LOG.info("No match: returning \"fresh\" 200 data")
+            ResponseEntity.ok()
+                    .lastModified(resourceLastModified).cacheControl(CacheControl.noCache())
+                    .body("Greetings using Last-Modified of " +
+                            "${Instant.ofEpochMilli(resourceLastModified)} at ${Instant.now()}")
+        } else {
+            LOG.info("Match: returning 304")
+            ResponseEntity.status(NOT_MODIFIED).build()
+        }
+    }
+
+    @RequestMapping("/etag-last-modified")
+    fun etagLastModified(swr: ServletWebRequest): ResponseEntity<String> {
+        LOG.info("GET /etag-last-modified with {} of {} and {} of {}", IF_MODIFIED_SINCE, swr.getHeader(IF_MODIFIED_SINCE),
+                IF_NONE_MATCH, swr.getHeader(IF_NONE_MATCH))
+        return if (!swr.checkNotModified(myEtag, resourceLastModified)) {
+            LOG.info("No match: returning \"fresh\" 200 data")
+            ResponseEntity.ok()
+                    .lastModified(resourceLastModified).eTag(myEtag).cacheControl(CacheControl.noCache())
+                    .body("Greetings using ETag $myEtag and Last-Modified of " +
+                            "${Instant.ofEpochMilli(resourceLastModified)} at ${Instant.now()}")
+        } else {
+            LOG.info("Match: returning 304")
+            ResponseEntity.status(NOT_MODIFIED).build()
+        }
+    }
+
+    @RequestMapping("/etag-1m")
+    fun etagOneMinute(swr: ServletWebRequest): ResponseEntity<String> {
+        LOG.info("GET /etag-1m with {} of {}", IF_NONE_MATCH, swr.getHeader(IF_NONE_MATCH))
+        return if (!swr.checkNotModified(myEtag)) {
+            LOG.info("No match: returning \"fresh\" 200 data")
+            ResponseEntity.ok()
+                    .eTag(myEtag).cacheControl(CacheControl.maxAge(1, MINUTES))
+                    .body("Greetings using ETag $myEtag and max-age of 1m at ${Instant.now()}")
+        } else {
+            LOG.info("Match: returning 304")
+            ResponseEntity.status(NOT_MODIFIED).build()
+        }
+    }
+
+    @RequestMapping("/last-modified-1m")
+    fun lastModifiedOneMinute(swr: ServletWebRequest): ResponseEntity<String> {
+        LOG.info("GET /last-modified-1m with {} of {}", IF_MODIFIED_SINCE, swr.getHeader(IF_MODIFIED_SINCE))
+        return if (!swr.checkNotModified(resourceLastModified)) {
+            LOG.info("No match: returning \"fresh\" 200 data")
+            ResponseEntity.ok()
+                    .lastModified(resourceLastModified).cacheControl(CacheControl.maxAge(1, MINUTES))
+                    .body("Greetings using Last-Modified of " +
+                            "${Instant.ofEpochMilli(resourceLastModified)} and max-age 1m at ${Instant.now()}")
+        } else {
+            LOG.info("Match: returning 304")
+            ResponseEntity.status(NOT_MODIFIED).build()
+        }
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<DemoApplication>(*args)
 }
+
+@Configuration
+open class MyConfiguration {
+    @Bean
+    open fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**").exposedHeaders("ETag")
+            }
+        }
+    }
+}
+
+val LOG: Logger = LoggerFactory.getLogger("Main")
+
+
+//@WebFilter(urlPatterns = ["/*"])
+//class HeaderLogger : Filter {
+//
+//    @Throws(ServletException::class)
+//    override fun init(filterConfig: FilterConfig) {
+//    }
+//
+//    @Throws(IOException::class, ServletException::class)
+//    override fun doFilter(request: ServletRequest, response: ServletResponse,
+//                 chain: FilterChain) {
+//        val req = request as HttpServletRequest
+//        val rep = response as HttpServletResponse
+//        println("----- Request ---------")
+//        Collections.list(req.getHeaderNames())
+//                .forEach { n -> println(n + ": " + req.getHeader(n)) }
+//
+//        chain.doFilter(request, response)
+//
+//        println("----- response ---------")
+//        rep.headerNames
+//                .forEach { n -> println(n + ": " + rep.getHeader(n)) }
+//
+//        println("response status: " + rep.status)
+//    }
+//
+//    override fun destroy() {}
+//}
